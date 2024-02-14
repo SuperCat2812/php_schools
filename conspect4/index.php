a670059b2c3495b7438d1ea79294b9fd128e5f19
SuperCat2812git config --global user.name SuperCat2812 <Sashka281205@gmail.com>
2024-02-14 18:58:17 +0100

    added 28_29

---
+++ conspect4/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require("../conspect4/fun_stuff.php");
+    // is_array($adventure);
+    // is_bool($adventure);
+    // is_object($adventure);
+    // is_int($adventure);
+    // is_float($adventure);
+    // echo gettype($adventure);
+    // echo gettype($adventure[0]);
+    // $adventure=json_decode(json_encode($adventure));
+    // echo gettype($adventure[0]->who);
+    // var_dump($adventure);
+    var_dump($adventure[0]['wat']);
+    ?>
+</body>
+
+</html>
---
+++ conspect5/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $text = 'this is words';
+    // echo str_replace(' ', '-', $text);
+    // echo str_replace(' ', '<br>', $text);
+// echo strlen($text);
+// if(strlen($text)<4){
+//     echo 'WRONG';
+// }
+    
+    // $url='http://balls.hu/penis';
+    
+    // echo '<pre>';
+// print_r($url);
+// echo'</pre>';
+// $url='http://balls.hu/penis';
+    
+    // echo '<pre>';
+// print_r(rtrim($url,'/'));
+// echo'</pre>';
+    
+    // $url='http://balls.hu/penis/';
+    
+    // echo '<pre>';
+// print_r(rtrim($url,'/'));
+// echo'</pre>';
+    
+    //Time
+// echo '<pre>';
+// print_r(time());
+// echo'</pre>';
+    
+    //Date
+// echo '<pre>';
+// print_r(date("F j, Y,g:i a"));
+// echo'</pre>';
+    
+    ?>
+</body>
+
+</html>
