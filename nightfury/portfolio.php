133abd4f8220c568cfc902b889512468883795be
SuperCat2812git config --global user.name SuperCat2812 <Sashka281205@gmail.com>
2024-04-27 19:16:25 +0200

    fix

--- nightfury/portfolio.php
+++ nightfury/portfolio.php
@@ -3,7 +3,7 @@
 $id = ($_GET['page']);
 
 
-$file = (glob('./file_portfolio/*.php', ));
+$file = (glob('./file_portfolio/*.php' ));
 
 foreach ($file as $fink) {
     if (basename($fink) == $id . ".php") {
